24a54ea0392667b820700a934c7aca41d39240c5
Paul-Bacher <paulbacher881@gmail.com>
2023-01-11 09:47:25 +0100

    F1-score pfbeta

---
+++ rsna_custom/rsna_custom/pfbeta.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def pfbeta(labels, predictions, beta=1):
+    y_true_count = 0
+    ctp = 0
+    cfp = 0
+
+    for idx in range(len(labels)):
+        prediction = min(max(predictions[idx], 0), 1)
+        if (labels[idx]):
+            y_true_count += 1
+            ctp += prediction
+        else:
+            cfp += prediction
+
+    beta_squared = beta * beta
+    c_precision = ctp / (ctp + cfp)
+    c_recall = ctp / y_true_count
+    
+    print("prec", c_precision)
+    print("reca", c_recall)
+    
+    if (c_precision > 0 and c_recall > 0):
+        result = (1 + beta_squared) * (c_precision * c_recall) / (beta_squared * c_precision + c_recall)
+        return result
+    else:
+        return 0
